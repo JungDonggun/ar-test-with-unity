e6deb66c4828afcfc87b0a9c41646e373d057115
JungDonggun <donggun.dev@gmail.com>
2021-11-08 20:46:19 +0900

    Add touchable detection script

--- Assets/Scripts/ARPlaceOnPlane.cs
+++ Assets/Scripts/ARPlaceOnPlane.cs
@@ -8,6 +8,7 @@ public class ARPlaceOnPlane : MonoBehaviour
 
     public ARRaycastManager arRaycastManager;
     public GameObject placeObject;
+    public GameObject spawnObject;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +19,34 @@ public class ARPlaceOnPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateCenterObject();
+        //UpdateCenterObject();
+        PlaceObjectByTouchable();
+    }
+
+    private void PlaceObjectByTouchable()
+    {
+        if (Input.touchCount > 0) //touchCount => 스마트폰 스크린에 터치를 몇 개 했는 지 => 검지 엄지 동시 => 2개
+        {
+            Touch touch = Input.GetTouch(0);
+
+            List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+            if (arRaycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes)) {
+                Pose hitPose = hits[0].pose;
+
+                if (!spawnObject)
+                {
+                    spawnObject = Instantiate(placeObject, hitPose.position, hitPose.rotation);
+                }
+                else
+                {
+                    spawnObject.transform.position = hitPose.position;
+                    spawnObject.transform.rotation = hitPose.rotation;
+                }
+
+            }
+
+        }
     }
 
     private void UpdateCenterObject()
