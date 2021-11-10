8da92dfcfde690374773c95f6053919880c561b0
JungDonggun <donggun.dev@gmail.com>
2021-11-10 16:46:37 +0900

    Add new scene

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+
+    public Text isSelectedSpanwer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +20,10 @@ public class UIController : MonoBehaviour
         
     }
 
-    public void onWorkAriSpawnerHandler()
+    public void donSomethingSpawner(string name)
     {
-        Debug.Log("do something! from onWorkAriSpawnerHandler");
-    }
-
-    public void onWorkSoonsoonSpawnerHandler()
-    {
-        Debug.Log("do something! from onWorkSoonsoonSpawnerHandler");
+        Debug.Log("IS CLICKED" + name);
+        isSelectedSpanwer.text = "IS SELECTED SPANWER IS " + name;
     }
 
 }
